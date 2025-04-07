78d7e5b20d2c4f22eca33ac5a6ba6e5d6fe6ec5a
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-04-08 00:43:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-// Creating a Person instance
-const person = new Person("John", 25);
-console.log(person.name);  // Output: John
+//getters and setters
 
-person.age = 30;           // Using the setter
-console.log(person.age);   // Output: 30
+//complete this code
+class Person {
+	 constructor(name, age) {
+    this._name = name; 
+    this._age = age;   
+  }
 
-// Creating a Student instance
-const student = new Student("Alice", 22);
-student.study();           // Output: Alice is studying
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+  set age(age) {
+    this._age = age;
+  }
+}
 
-// Creating a Teacher instance
-const teacher = new Teacher("Bob", 40);
-teacher.teach();           // Output: Bob is teaching
+class Student extends Person {
+  constructor(name, age) {
+    super(name, age); 
+  }
+  study() {
+    console.log(${this.name} is studying);
+  }
+}
+
+class Teacher extends Person {
+ constructor(name, age) {
+    super(name, age);
+  }
+  teach() {
+    console.log(${this.name} is teaching);
+  }
+}
+// Do not change the code below this line
+window.Person = Person;
+window.Student = Student;
+window.Teacher = Teacher;
